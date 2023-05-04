fccb4875ff905824deef08bcdb9078728528aea5
Josh Parina <joshua.parina@gmail.com>
2023-05-04 10:33:33 -0500

    added .vite to .gitignore + json response to views.py + minor changes to settings.py

--- volunteer_coordinator/volunteer_backend/views.py
+++ volunteer_coordinator/volunteer_backend/views.py
@@ -3,22 +3,29 @@ from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 from .forms import CustomUserCreationForm
-from .models import CustomUser
+from django.views.decorators.csrf import csrf_protect
 from .serializers import CustomUserSerializer
+from django.forms.utils import ErrorDict
 
+@csrf_protect
 @api_view(['POST'])
 def register(request):
-    print(request.data)
-    form = CustomUserCreationForm(request.data)
-    if form.is_valid():
-        user = form.save()
-        login(request, user)
-        user_serializer = CustomUserSerializer(user)
-        # print(user_serializer)
-        return JsonResponse({"detail": "User registered and logged in successfully.", 'current_user': user_serializer.data}, status=status.HTTP_201_CREATED)
-    return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
+    try:
+        form = CustomUserCreationForm(request.data)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            user_serializer = CustomUserSerializer(user)
+            return JsonResponse({"detail": "User registered and logged in successfully.", 'current_user': user_serializer.data}, status=status.HTTP_201_CREATED)
+        else:
+            raise ValidationError("Form is not valid.")
+    except ValidationError as e:
+        form_errors = ErrorDict(form.errors).as_json()
+        return Response({"detail": str(e), "form_errors": form_errors}, status=status.HTTP_400_BAD_REQUEST)
 
+@csrf_protect
 @api_view(['POST'])
 def login_view(request):
     email = request.data.get('email')
@@ -29,7 +36,7 @@ def login_view(request):
         return Response({"detail": "User logged in successfully."}, status=status.HTTP_200_OK)
     return Response({"detail": "Invalid email or password."}, status=status.HTTP_400_BAD_REQUEST)
 
-
+@csrf_protect
 @api_view(['POST'])
 def logout_view(request):
     logout(request)
