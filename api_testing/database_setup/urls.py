1de54aeb0dd5365e4040be854c999619ef6549be
Josh Parina <joshua.parina@gmail.com>
2023-05-11 13:07:21 -0500

    changed path from event to create-event

--- api_testing/database_setup/urls.py
+++ api_testing/database_setup/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from database_setup.views import *
 urlpatterns = [
      path('volunteer-signup/', VolunteerSignUpView.as_view(), name='signup'),
-     path('event/', EventAPIView.as_view(), name='event'),
+     path('create-event/', EventAPIView.as_view(), name='event'),
 ]
