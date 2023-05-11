566cba3a8149ecc699b3c1526a626a064be825a7
Josh Parina <joshua.parina@gmail.com>
2023-05-10 23:38:12 -0500

    edited volunteer model to use greedy algorithm

--- api_testing/database_setup/models.py
+++ api_testing/database_setup/models.py
@@ -1,102 +1,7 @@
-# from django.db import models
-
-
-# class Activity(models.Model):
-#     class Meta:
-#         verbose_name_plural = 'Activities'
-        
-#     name = models.CharField(max_length=100)
-
-#     def __str__(self):
-#         return self.name
-
-    
-        
-# class Event(models.Model):
-#     name = models.CharField(max_length=100)
-#     date_of_event = models.DateField()
-#     start_time = models.TimeField()
-#     end_time = models.TimeField()
-#     activities = models.ManyToManyField(Activity, through='EventActivity')
-
-#     def __str__(self):
-#         return self.name
-
-
-# class Availability(models.Model):
-#     class Meta:
-#         verbose_name_plural = 'Availabilities'
-        
-#     DAYS_OF_WEEK = [
-#         (0, 'Monday'),
-#         (1, 'Tuesday'),
-#         (2, 'Wednesday'),
-#         (3, 'Thursday'),
-#         (4, 'Friday'),
-#         (5, 'Saturday'),
-#         (6, 'Sunday'),
-#     ]
-
-#     volunteer = models.ForeignKey('Volunteer', on_delete=models.CASCADE)
-#     day_of_week = models.IntegerField(choices=DAYS_OF_WEEK)
-#     start_time = models.TimeField()
-#     end_time = models.TimeField()
-
-#     def overlaps(self, other_start, other_end):
-#         return self.start_time < other_end and self.end_time > other_start
-
-
-# class Volunteer(models.Model):
-#     name = models.CharField(max_length=100)
-#     desired_activities = models.ManyToManyField(Activity)
-
-#     def __str__(self):
-#         return self.name
-
-#     def assign_to_shift(self):
-#         for availability in self.availability_set.all():
-#             for activity in self.desired_activities.all():
-#                 for event_activity in activity.eventactivity_set.all():
-#                     if event_activity.date_of_event.weekday() == availability.day_of_week and availability.overlaps(event_activity.start_time, event_activity.end_time) and event_activity.volunteers_needed_at(availability.start_time, availability.end_time) > 0:
-#                         VolunteerShift.objects.create(
-#                             event_activity=event_activity,
-#                             volunteer=self,
-#                             start_time=max(availability.start_time, event_activity.start_time),
-#                             end_time=min(availability.end_time, event_activity.end_time),
-#                         )
-
-
-# class EventActivity(models.Model):
-#     class Meta:
-#         verbose_name_plural = 'Event Activities'
-#     event = models.ForeignKey(Event, on_delete=models.CASCADE)
-#     activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
-#     date_of_event = models.DateField()
-#     start_time = models.TimeField()
-#     end_time = models.TimeField()
-#     required_volunteers = models.IntegerField()
-#     volunteers = models.ManyToManyField(Volunteer, through='VolunteerShift', blank=True)
-
-#     def __str__(self):
-#         return f'{self.event.name} - {self.activity.name}'
-
-#     def volunteers_needed_at(self, start_time, end_time):
-#         shifts_at_time = self.volunteershift_set.filter(start_time__lte=end_time, end_time__gt=start_time)
-#         return self.required_volunteers - shifts_at_time.count()
-
-
-# class VolunteerShift(models.Model):
-#     event_activity = models.ForeignKey(EventActivity, on_delete=models.CASCADE)
-#     volunteer = models.ForeignKey(Volunteer, on_delete=models.CASCADE)
-#     start_time = models.TimeField()
-#     end_time = models.TimeField()
-
-#     def __str__(self):
-#         return f'{self.volunteer.name} - {self.event_activity} - {self.start_time} to {self.end_time}'
 from django.db import models
 from datetime import timedelta
 from datetime import datetime, date
-from django.db.models import DurationField, ExpressionWrapper
+from django.db.models import F, ExpressionWrapper, fields
 
 def time_to_datetime(t, d=date.today()):
     return datetime.combine(d, t)
@@ -159,6 +64,7 @@ class Volunteer(models.Model):
 
     def assign_to_shift(self):
         availabilities = self.availability_set.all()
+        possible_assignments = []
         for availability in availabilities:
             event_activities = EventActivity.objects.filter(
                 date_of_event__week_day=availability.day_of_week + 1,  # Django week_day starts from 1 (Sunday)
@@ -167,13 +73,53 @@ class Volunteer(models.Model):
                 activity__in=self.desired_activities.all()
             ).order_by('start_time')
             for event_activity in event_activities:
-                if event_activity.volunteers_needed_at(availability.start_time, availability.end_time) > 0:
-                    VolunteerShift.objects.create(
-                        volunteer=self,
-                        event_activity=event_activity,
-                        start_time=max(event_activity.start_time, availability.start_time),
-                        end_time=min(event_activity.end_time, availability.end_time)
-                    )
+                if event_activity.volunteers_needed_at(availability.start_time, availability.end_time).total_seconds() > 0:
+                    possible_assignments.append({
+                        'availability': availability,
+                        'event_activity': event_activity,
+                        'start_time': max(event_activity.start_time, availability.start_time),
+                        'end_time': min(event_activity.end_time, availability.end_time)
+                    })
+        
+        best_assignments = self.select_best_assignments(possible_assignments)
+        for assignment in best_assignments:
+            VolunteerShift.objects.create(
+                volunteer=self,
+                event_activity=assignment['event_activity'],
+                start_time=assignment['start_time'],
+                end_time=assignment['end_time']
+            )
+    def select_best_assignments(self, assignments):
+            sorted_assignments = sorted(assignments, key=lambda x: x['end_time'])
+            best_assignments = []
+            current_end_time = None
+            for assignment in sorted_assignments:
+                if current_end_time is None or assignment['start_time'] >= current_end_time:
+                    best_assignments.append(assignment)
+                    current_end_time = assignment['end_time']
+            return best_assignments    
+    def total_assigned_time(self):
+        shifts = self.volunteershift_set.all()
+        total_time = timedelta()
+        for shift in shifts:
+            total_time += time_to_datetime(shift.end_time) - time_to_datetime(shift.start_time)
+        return total_time
+        
+    def is_available(self, start_time, end_time):
+        availabilities = self.availability_set.filter(
+            start_time__lte=end_time,
+            end_time__gte=start_time,
+        )
+        if not availabilities:
+            return False
+        shifts = self.volunteershift_set.filter(
+            start_time__lt=end_time, 
+            end_time__gt=start_time
+        )
+        total_shift_time = timedelta()
+        for shift in shifts:
+            total_shift_time += min(shift.end_time, end_time) - max(shift.start_time, start_time)
+        return total_shift_time < (end_time - start_time)
 
 
 class EventActivity(models.Model):
@@ -195,16 +141,14 @@ class EventActivity(models.Model):
             start_time__lt=end_time, 
             end_time__gt=start_time
         )
+        shifts = shifts.annotate(
+            duration=ExpressionWrapper(F('end_time') - F('start_time'), output_field=fields.DurationField())
+        )
         total_volunteers = shifts.aggregate(
-            total=models.Sum(
-                models.ExpressionWrapper(
-                    time_to_datetime(models.F('end_time')) - time_to_datetime(models.F('start_time')), 
-                    output_field=models.DurationField()
-                )
-            )
+            total=models.Sum('duration')
         )['total'] or timedelta()
-        required_time = (time_to_datetime(self.end_time) - time_to_datetime(self.start_time)) * self.required_volunteers
-        return required_time - total_volunteers
+        required_time = (time_to_datetime(end_time) - time_to_datetime(start_time)) * self.required_volunteers
+        return max(timedelta(), required_time - total_volunteers)
 
 
 
