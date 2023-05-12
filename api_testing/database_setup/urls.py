cb5e560ea8731374d6dc7673302d941fde215c21
Josh Parina <joshua.parina@gmail.com>
2023-05-12 15:50:35 -0500

    added eventActivityAPI view and volunteerShiftAPI view to send and receive data for event page

--- api_testing/database_setup/urls.py
+++ api_testing/database_setup/urls.py
@@ -3,6 +3,6 @@ from database_setup.views import *
 urlpatterns = [
      path('volunteer-signup/', VolunteerSignUpView.as_view(), name='signup'),
      path('event/', EventAPIView.as_view(), name='event'),
-     path('event-activity/', EventActivity.as_view(), name='event-activity'),
-     path('volunteer-shift/', VolunteerShifts.as_view(), name='volunteer-shifts')
+     path('event-activity/', EventActivityAPIView.as_view(), name='event-activity'),
+     path('volunteer-shift/', VolunteerShiftsAPIview.as_view(), name='volunteer-shifts')
 ]
