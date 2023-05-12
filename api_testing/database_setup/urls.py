aca8f86bb193b88772eaf0ba30e181425e585bf8
Josh Parina <joshua.parina@gmail.com>
2023-05-12 12:44:24 -0500

    created api endpoint for event get request

--- api_testing/database_setup/urls.py
+++ api_testing/database_setup/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from database_setup.views import *
 urlpatterns = [
      path('volunteer-signup/', VolunteerSignUpView.as_view(), name='signup'),
-     path('create-event/', EventAPIView.as_view(), name='event'),
+     path('event/', EventAPIView.as_view(), name='event'),
 ]
