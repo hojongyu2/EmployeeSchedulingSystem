e7871cc925303b832df8978ed66298d88d101a3d
Josh Parina <joshua.parina@gmail.com>
2023-05-12 14:22:09 -0500

    added url to send post request to send eventID to receive associated eventActivities

--- api_testing/database_setup/urls.py
+++ api_testing/database_setup/urls.py
@@ -3,4 +3,5 @@ from database_setup.views import *
 urlpatterns = [
      path('volunteer-signup/', VolunteerSignUpView.as_view(), name='signup'),
      path('event/', EventAPIView.as_view(), name='event'),
+     path('event-activity/', EventActivity.as_view(), name='event-activity')
 ]
