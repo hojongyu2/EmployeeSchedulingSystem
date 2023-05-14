fe288b104221696928d9d05f8bc3c4033a08cd0f
Josh Parina <joshua.parina@gmail.com>
2023-05-14 15:16:27 -0500

    commented new paths that link to volunteer response confirmation

--- api_testing/database_setup/urls.py
+++ api_testing/database_setup/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
      path('event-activity/', EventActivityAPIView.as_view(), name='event-activity'),
      path('volunteer-shift/', VolunteerShiftsAPIview.as_view(), name='volunteer-shifts'),
      path('confirm-shift/<int:shift_id>/<str:confirmed_value>/', ConfirmShiftView.as_view(), name='confirm-shift'),
-     # connect if response = no, delete volunteer shift
+     # path('shift-confirmed/, insert view here, name=confirmed)
+     # path('shift-rejected/' insert view here, name=rejected)
 ]
