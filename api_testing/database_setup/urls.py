f7366e3279f5f3f632266709d006f040ebbd7ae8
Josh Parina <joshua.parina@gmail.com>
2023-05-14 09:59:09 -0500

    added path for email confirmation

--- api_testing/database_setup/urls.py
+++ api_testing/database_setup/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
      path('event/', EventAPIView.as_view(), name='event'),
      path('event-activity/', EventActivityAPIView.as_view(), name='event-activity'),
      path('volunteer-shift/', VolunteerShiftsAPIview.as_view(), name='volunteer-shifts'),
-     path('confirm-shift/<int:shift_id>/<str:confirmed_value>/', ConfirmShiftView.as_view(), name='confirm-shift')
-
+     path('confirm-shift/<int:shift_id>/<str:confirmed_value>/', ConfirmShiftView.as_view(), name='confirm-shift'),
+     # connect if response = no, delete volunteer shift
 ]
