b39feee38bcfda7e1bd93d32f7ae4ee476709a6f
dan-zayas <dmzayasplus@gmail.com>
2023-05-15 09:45:03 -0500

    fixed variable view for profile dropdown

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,187 +1,8 @@
-// import React, { useContext, useState } from 'react';
-// import { userSignOut } from '../../utilities/userAuthAxios';
-// import { useNavigate } from 'react-router-dom';
-// import { userContext } from '../context/UserContext';
-// import { Link } from 'react-router-dom';
-// import {
-//   AppBar,
-//   Box,
-//   Toolbar,
-//   IconButton,
-//   Typography,
-//   Menu,
-//   Container,
-//   Avatar,
-//   Tooltip,
-//   MenuItem,
-//   Icon,
-//   ListItemIcon,
-//   useMediaQuery,
-// } from '@mui/material';
-// import MenuIcon from '@mui/icons-material/Menu';
-// import AdbIcon from '@mui/icons-material/Adb';
-
-// const Header = () => {
-//   const isDesktop = useMediaQuery('(min-width: 960px)');
-//   const { user, setUser } = useContext(userContext);
-//   const navigate = useNavigate();
-
-//   const [anchorElNav, setAnchorElNav] = useState(null);
-//   const [anchorElUser, setAnchorElUser] = useState(null);
-
-//   const handleOpenNavMenu = (event) => {
-//     setAnchorElNav(event.currentTarget);
-//   };
-
-//   const handleOpenUserMenu = (event) => {
-//     setAnchorElUser(event.currentTarget);
-//   };
-
-//   const handleCloseNavMenu = () => {
-//     setAnchorElNav(null);
-//   };
-
-//   const handleCloseUserMenu = () => {
-//     setAnchorElUser(null);
-//   };
-
-//   const handleMenuCloseAndLogIn = () => {
-//     navigate('/login');
-//   };
-
-//   const handleMenuCloseAndLogout = async () => {
-//     const res = await userSignOut();
-//     if (res.detail === 'Logged out successfully') {
-//       localStorage.clear();
-//       setUser('');
-//       navigate('/');
-//     }
-//   };
-
-//   return (
-//     <AppBar position="static">
-//       <Container maxWidth="xl">
-//         <Toolbar disableGutters>
-//           <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-//           <Typography
-//             variant="h6"
-//             noWrap
-//             component={Link}
-//             to="/"
-//             sx={{
-//               mr: 2,
-//               display: { xs: 'none', md: 'flex' },
-//               fontFamily: 'monospace',
-//               fontWeight: 700,
-//               letterSpacing: '.3rem',
-//               color: 'inherit',
-//               textDecoration: 'none',
-//             }}
-//           >
-//             SHIFT
-//           </Typography>
-//           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-//             <IconButton
-//               size="large"
-//               aria-label="account of current user"
-//               aria-controls="menu-appbar"
-//               aria-haspopup="true"
-//               onClick={handleOpenNavMenu}
-//               color="inherit"
-//             >
-//               <MenuIcon />
-//             </IconButton>
-//           </Box>
-
-//           <Typography>{user && user.first_name}</Typography>
-
-//           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-//             <Menu
-//               id="menu-appbar"
-//               anchorEl={anchorElNav}
-//               anchorOrigin={{
-//                 vertical: 'bottom',
-//                 horizontal: 'left',
-//               }}
-//               keepMounted
-//               transformOrigin={{
-//                 vertical: 'top',
-//                 horizontal: 'left',
-//               }}
-//               open={Boolean(anchorElNav)}
-//               onClose={handleCloseNavMenu}
-//             >
-//               <MenuItem component={Link} to="/create-event">
-//                 <ListItemIcon>
-//                   <Icon>people</Icon>
-//                 </ListItemIcon>
-//                 Create Event
-//               </MenuItem>
-//               <MenuItem component={Link} to="/request">
-//                 <ListItemIcon>
-//                   <Icon>people</Icon>
-//                 </ListItemIcon>
-//                 Send Request
-//               </MenuItem>
-//               <MenuItem component={Link} to="/">
-//                 <ListItemIcon>
-//                   <Icon>people</Icon>
-//                 </ListItemIcon>
-//                 Student List
-//               </MenuItem>
-//             </Menu>
-//           </Box>
-
-//           <Box sx={{ flexGrow: 0 }}>
-//             <Tooltip title="Open settings">
-//               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-//                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-//               </IconButton>
-//             </Tooltip>
-//             <Menu
-//               sx={{ mt: '45px' }}
-//               id="menu-appbar"
-//               anchorEl={anchorElUser}
-//               anchorOrigin={{
-//                 vertical: 'top',
-//                 horizontal: 'right',
-//               }}
-//               keepMounted
-//               transformOrigin={{
-//                 vertical: 'top',
-//                 horizontal: 'right',
-//               }}
-//               open={Boolean(anchorElUser)}
-//               onClose={handleCloseUserMenu}
-//             >
-//               {!user && (
-//                 <MenuItem onClick={handleMenuCloseAndLogIn}>
-//                   <Typography textAlign="center">Log In</Typography>
-//                 </MenuItem>
-//               )}
-//               {user && (
-//                 <MenuItem onClick={handleMenuClose}>
-//                   <Typography textAlign="center">Profile</Typography>
-//                 </MenuItem>
-//               )}
-//               {user && (
-//                 <MenuItem onClick={handleMenuCloseAndLogout}>
-//                   <Typography textAlign="center">Log Out</Typography>
-//                 </MenuItem>
-//               )}
-//             </Menu>
-//           </Box>
-
-//         </Toolbar>
-//       </Container>
-//     </AppBar>
-//   );
-// };
-
-// export default Header;
-
-
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { userContext } from '../context/userContext';
+
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -197,11 +18,10 @@ import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import Icon from '@mui/material/Icon';
 import ListItemIcon from '@mui/material/ListItemIcon';
-import { Link } from 'react-router-dom';
 
 const pageUrls = {
   'Create Event': '/create-event',
-  'Send Request': '/request'
+  'Volunteer Signup': '/request'
 };
 
 const profileDropdown = {
@@ -220,6 +40,8 @@ const profileDropdown = {
 };
 
 function ResponsiveAppBar() {
+  const { user, setUser } = useContext(userContext)
+
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
@@ -320,16 +142,27 @@ function ResponsiveAppBar() {
             SHIFT
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {Object.keys(pageUrls).map((page, url) => (
-              <Button
-                key={page}
-                sx={{ my: 2, display: 'block' }}
-              >
-                <Link to={pageUrls[page]}>
-                  {page}
-                </Link>
-              </Button>
-            ))}
+            {Object.keys(pageUrls).map((page, i) => {
+              if (page !== 'Create Event') {
+                <Button
+                  key={page}
+                  sx={{ my: 2, display: 'block' }}
+                >
+                  <Link to={pageUrls[page]}>
+                    {page}
+                  </Link>
+                </Button>
+              } else if (user) {
+                <Button
+                  key={page}
+                  sx={{ my: 2, display: 'block' }}
+                >
+                  <Link to={pageUrls[page]}>
+                    {page}
+                  </Link>
+                </Button>
+              }
+            })}
             
           </Box>
 
@@ -355,11 +188,22 @@ function ResponsiveAppBar() {
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
-              {Object.keys(profileDropdown).map((name, action) => (
-                <MenuItem key={name} onClick={action}>
-                  <Typography textAlign="center">{name}</Typography>
-                </MenuItem>
-              ))}
+              {Object.keys(profileDropdown).map((name, index) => {
+                if (user && name !== 'Log In') {
+                  return (
+                    <MenuItem key={name} onClick={profileDropdown[name]}>
+                      <Typography textAlign="center">{name}</Typography>
+                    </MenuItem>
+                  );
+                }
+                else if (name === 'Log In') {
+                  return (
+                    <MenuItem key={name} onClick={profileDropdown[name]}>
+                      <Typography textAlign="center">{name}</Typography>
+                    </MenuItem>
+                  );
+                }
+              })}
             </Menu>
           </Box>
         </Toolbar>
